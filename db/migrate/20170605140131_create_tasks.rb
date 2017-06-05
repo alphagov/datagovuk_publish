bdefedd88f8fba6fa005142eda5869dcd01c4b8e
hannako <jessica.jones@digital.cabinet-office.gov.uk>
2017-06-05 15:04:53 +0100

    creates task model

---
+++ db/migrate/20170605140131_create_tasks.rb
@@ -0,0 +1,15 @@
+class CreateTasks < ActiveRecord::Migration[5.1]
+  def change
+    create_table :tasks do |t|
+      t.references :organisation, foreign_key: true
+      t.integer :quantity, default: 0
+      t.string :required_permission_name
+      t.string :description, limit: 128, default: "", null: true
+      t.string :category, limit: 20
+  #TODO rename to short_name?
+      t.string :owning_organisation, limit: 128, null: true
+      t.uuid :related_object_id, null: true
+      t.timestamps
+    end
+  end
+end
