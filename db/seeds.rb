846fcdbb59a418d0cb51a96670f7b1436ee48dee
hannako <jessica.jones@digital.cabinet-office.gov.uk>
2017-06-05 16:39:41 +0100

    adds some seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,40 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
+admin = AdminUser.create!(
+  email: 'admin@example.com',
+  password: 'password',
+  password_confirmation: 'password')
+
+publisher = PublishingUser.create!(
+  email: 'publisher@example.com',
+  password: 'password',
+  password_confirmation: 'password',
+  primary_organisation: Organisation.new(
+    name: 'Land_Registry'
+  )
+)
+
+fix_task = Task.create!(
+  organisation: Organisation.new,
+  description: 'fix this task'
+)
+
+update_task = Task.create!(
+  organisation: Organisation.new,
+  description: 'update this task'
+)
+
+price_paid_dataset = Dataset.create!(
+  name: 'price paid data',
+  title: 'Price Paid data for all London Boroughs',
+  summary: 'Price Paid Data tracks the residential property sales in England and Wales that are lodged with HM Land Registry for registration. ',
+  organisation: Organisation.new
+)
+
+HMRC_spending_dataset = Dataset.create!(
+  name: 'HMRC spending',
+  title: 'HMRC spending over £25000',
+  summary: 'Monthly details of HMRC’s spending with suppliers covering transactions over £25,000',
+  organisation: Organisation.new
+)
