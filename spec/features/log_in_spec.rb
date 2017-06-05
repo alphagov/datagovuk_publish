c218080cfc0a64b827d00aa92fb1faf26482432f
Laurence <laurence.smith@digital.cabinet-office.gov.uk>
2017-06-05 11:51:19 +0100

    Get PublishingUser and Organisation somewhat working, add a dumb test

--- spec/features/log_in_spec.rb
+++ spec/features/log_in_spec.rb
@@ -2,10 +2,14 @@ require 'rails_helper'
 
 describe "logging in" do
   before(:each) do
-
+    o = Organisation.create!
+    PublishingUser.create!(email:'test@localhost',
+                           primary_organisation: o,
+                           password: 'password',
+                           password_confirmation: 'password')
   end
 
-  it "can successfully log in" do
+  it "can visit the index page" do
     visit '/'
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,6 @@
 require "simplecov"
 SimpleCov.start
 
-require 'capybara/rails'
-require 'capybara/rspec'
-
 RSpec.configure do |config|
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
