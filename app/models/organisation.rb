aafe945599726e41dd196b4315352c221f810458
Laurence <laurence.smith@digital.cabinet-office.gov.uk>
2017-06-06 14:58:45 +0100

    Fix destroy dependencies between user, org, and model

--- app/models/organisation.rb
+++ app/models/organisation.rb
@@ -1,6 +1,9 @@
 class Organisation < ApplicationRecord
-  has_and_belongs_to_many :publishing_users
   audited
+  has_and_belongs_to_many :publishing_users
+  has_many :tasks, dependent: :destroy
+
+  before_destroy :deregister_users
 
   def active?
     active
@@ -9,4 +12,13 @@ class Organisation < ApplicationRecord
   def closed?
     !active?
   end
+
+  private
+  def deregister_users
+    publishing_users.where(primary_organisation: self).each do |p|
+      p.primary_organisation = nil
+      p.save!
+    end
+  end
+
 end
