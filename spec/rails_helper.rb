4242b99f8112572ce7af2bb1c581b5a28042b08f
hannako <jessica.jones@digital.cabinet-office.gov.uk>
2017-06-06 12:43:50 +0100

    adds Devise test helpers to rails_helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,6 +5,7 @@ require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
+require 'devise'
 require 'capybara/rspec'
 require 'capybara/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
@@ -31,6 +32,7 @@ ActiveRecord::Migration.maintain_test_schema!
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  config.include Devise::Test::ControllerHelpers
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
