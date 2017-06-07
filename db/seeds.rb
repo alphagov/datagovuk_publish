64abf4dac68913451d5886718f381525cd7b2afa
Ross Jones <ross@servercode.co.uk>
2017-06-07 13:33:03 +0100

    Use a real org in the task page
    Changes the seeds to use a real organisation (which may be updated after
    organisation import) and then uses those details rather than all datasets.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,27 +5,34 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# We can create land-registry now, and if we import organisations
+# then they will just update it.
+land_registry = Organisation.new
+land_registry.name = "land-registry"
+land_registry.title = "Land Registry"
+land_registry.save!()
+
 admin = AdminUser.create!(
   email: 'admin@example.com',
   password: 'password',
-  password_confirmation: 'password')
+  password_confirmation: 'password'
+)
 
 publisher = User.create!(
   email: 'publisher@example.com',
   password: 'password',
   password_confirmation: 'password',
-  primary_organisation: Organisation.new(
-    name: 'Land Registry'
-  )
+  primary_organisation: land_registry
 )
 
 fix_task = Task.create!(
-  organisation: Organisation.new,
+  organisation: land_registry,
   description: 'fix this task'
 )
 
 update_task = Task.create!(
-  organisation: Organisation.new,
+  organisation: land_registry,
   description: 'update this task'
 )
 
@@ -33,19 +40,19 @@ price_paid_dataset = Dataset.create!(
   name: 'price paid data',
   title: 'Price Paid data for all London Boroughs',
   summary: 'Price Paid Data tracks the residential property sales in England and Wales that are lodged with HM Land Registry for registration. ',
-  organisation: Organisation.new
+  organisation: land_registry
 )
 
 hmrc_spending_dataset = Dataset.create!(
   name: 'HMRC spending',
   title: 'HMRC spending over £25000',
   summary: 'Monthly details of HMRC’s spending with suppliers covering transactions over £25,000',
-  organisation: Organisation.new
+  organisation: land_registry
 )
 
 council_tax_bands = Dataset.create!(
   name: 'Council Tax',
   title: 'Council Tax bands for London',
   summary: 'Council tax bands for the current year',
-  organisation: Organisation.new
+  organisation: land_registry
 )
