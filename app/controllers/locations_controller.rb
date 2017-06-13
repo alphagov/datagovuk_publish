9a80d373e38410046bc61ba8012c6f611852d083
Max Froumentin <max.froumentin@digital.cabinet-office.gov.uk>
2017-06-13 16:07:24 +0100

    Add a new route to query locations by substring

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -3,7 +3,15 @@ class LocationsController < ApplicationController
   protect_from_forgery prepend: :true
   before_action :authenticate_user!
 
+
   def lookup
-    Location.all
+    if params[:q]
+      @query = params[:q].downcase
+      @locations = Location.where("name ILIKE CONCAT('%',:search,'%')", {search: @query})
+      @location_strings = @locations.map { | l | l.name + " (" + l.location_type + ")" }
+      render json: @location_strings
+    else
+      render json: []
+    end
   end
 end
