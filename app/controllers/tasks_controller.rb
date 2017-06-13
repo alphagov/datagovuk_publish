4be6fc228dbb9f18868bc8f4137dce49fef35882
hannako <jessica.jones@digital.cabinet-office.gov.uk>
2017-06-13 11:50:12 +0100

    implement sort on tasks page

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,22 +3,54 @@ class TasksController < ApplicationController
   protect_from_forgery prepend: :true
   before_action :authenticate_user!
 
+
   def my
     @organisation = current_user.primary_organisation
     @datasets = Dataset.where(organisation: @organisation.id)
+    @broken_datasets = Dataset.where(organisation: @organisation.id)
     @tasks = get_tasks_for_user(current_user)
-    @datasetsBroken = @datasets.all
+    manage_sort
   end
 
 
   def organisation
     @organisation = current_user.primary_organisation
     @datasets = Dataset.where(organisation: @organisation.id)
+    @broken_datasets = Dataset.where(organisation: @organisation.id)
     @tasks = get_tasks_for_organisation(@organisation.name)
-    @datasetsBroken = @datasets
+    manage_sort
   end
 
 private
+
+def manage_sort
+
+  @update_date_sort   = "descending"
+  @update_name_sort   = "name"
+  @fix_name_sort      = "broken-name"
+  @fix_count_sort     = "decreasing"
+
+  case params["sort_by"]
+
+  when "descending"
+    @update_date_sort = "ascending"
+  when "ascending"
+    @update_date_sort = "descending"
+  when "name"
+    @update_name_sort = "-name"
+  when "-name"
+    @update_name_sort = "name"
+  when "broken-name"
+    @fix_name_sort = "-broken-name"
+  when "-broken-name"
+    @fix_name_sort = "broken-name"
+  when "decreasing"
+    @fix_count_sort = "increasing"
+  else
+    @fix_count_sort = "decreasing"
+  end
+
+end
 
   def get_tasks_for_user(_user)
     Task.all
--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -1,2 +1,33 @@
 module TasksHelper
+
+  def sorted_update_tasks
+    if params["sort_by"] == "ascending"
+      @tasks.order(created_at: :asc)
+    elsif params["sort_by"] == "-name"
+      @tasks.order(description: :desc)
+    elsif params["sort_by"] == "name"
+      @tasks.order(:description)
+    else
+      @tasks.order(created_at: :desc)
+    end
+  end
+
+  def sorted_fix_tasks
+    if params["sort_by"] == "-broken-name"
+      @broken_datasets.order(title: :desc)
+    elsif params["sort_by"] == "decreasing"
+      #TODO: should be ordered by broken link count, created_at just a place holder
+    @broken_datasets.order(created_at: :desc)
+    elsif params["sort_by"] == "increasing"
+      # TODO: should be ordered by broken link count, created_at just a place holder
+      @broken_datasets.order(created_at: :asc)
+    else
+      @broken_datasets.order(title: :asc)
+    end
+  end
+
+  def broken_dataset_count
+    @broken_datasets.count
+  end
+
 end
