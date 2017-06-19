3f26f89fc118356810f2cc2bb99df073e6228e11
Laurence <laurence.smith@digital.cabinet-office.gov.uk>
2017-06-19 15:14:19 +0100

    Add date processing for datafiles

--- app/controllers/datasets_controller.rb
+++ app/controllers/datasets_controller.rb
@@ -57,6 +57,7 @@ class DatasetsController < ApplicationController
       file_params = params.require(:datafile).permit(:url, :name)
       @datafile = Datafile.new(file_params)
       @datafile.dataset = @dataset
+      set_dates(params.require(:datafile).permit(DATE_PARAMS))
 
       if @datafile.save
         redirect_to new_adddoc_dataset_path(@dataset)
@@ -94,6 +95,58 @@ class DatasetsController < ApplicationController
   ]
 
   private
+  DATE_PARAMS = [
+    :quarter,
+    :year,
+    :start_day,
+    :start_month,
+    :start_year,
+    :end_day,
+    :end_month,
+    :end_year
+  ]
+
+  # TODO NEEDS TESTS!
+  def set_dates(date_params)
+    if @dataset.weekly? || @dataset.monthly?
+      @datafile.start_date = start_date(date_params)
+    end
+
+    if @dataset.weekly?
+      @datafile.end_date = end_date(date_params)
+    elsif @dataset.monthly?
+      @datefile.end_date = @datafile.start_date + 1.month
+    end
+
+    if @dataset.quarterly?
+      @datafile.start_date = quarter(date_params)
+    end
+
+    if @dataset.annually?
+      @datafile.start_date = Date.new(date_params[:year])
+      @datafile.end_date = Date.new(date_params[:year]) + 1.year
+    end
+
+    if @dataset.financial_yearly?
+      @datafile.start_date = Date.new(date_params[:year], 4, 1)
+      @datafile.end_date = Date.new(date_params[:year], 4, 1) + 1.year
+    end
+  end
+
+  def start_date(date_params)
+    Date.new(date_params[:start_year], date_params[:start_month], date_params[:start_day])
+  end
+
+  def end_date(date_params)
+    Date.new(date_params[:end_year], date_params[:end_month], date_params[:end_day])
+  end
+
+  def quarter(date_params)
+    year_start = Date.new(date_params[:year], 1, 1)
+    quarter_offset = 4 + (data_params[:quarter] - 1) * 3 # Q1: 4, Q2: 7, Q3: 10, Q4: 13
+    year_start + quarter_offset.months
+  end
+
   def get_licence(dataset_params)
     if dataset_params[:licence] == 'other'
       return dataset_params[:licence_other]
