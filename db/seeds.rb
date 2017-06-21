5d77fca6118cf69f5f6c4b2b32a920b2f57d5bf1
Laurence <laurence.smith@digital.cabinet-office.gov.uk>
2017-06-21 10:58:45 +0100

    Clean up seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,14 +18,16 @@ hmrc.name = "hmrc"
 hmrc.title = "hmrc"
 hmrc.save!()
 
-admin = AdminUser.create!(
+# Admin
+AdminUser.create!(
   email: 'admin@example.com',
   name: 'Administrator',
   password: 'password',
   password_confirmation: 'password'
 )
 
-lr_publisher = User.create!(
+# Land Registry User
+User.create!(
   email: 'publisher@example.com',
   name: 'Publisher',
   password: 'password',
@@ -33,7 +35,8 @@ lr_publisher = User.create!(
   primary_organisation: land_registry
 )
 
-hmrc_publisher = User.create!(
+# HMRC User
+User.create!(
   email: 'hmrc_publisher@example.com',
   name: 'HMRC',
   password: 'password',
@@ -41,47 +44,52 @@ hmrc_publisher = User.create!(
   primary_organisation: hmrc
 )
 
-fix_task = Task.create!(
+# Fix task
+Task.create!(
   organisation: land_registry,
   description: 'fix this task'
 )
 
-update_task = Task.create!(
+# Update Task
+Task.create!(
   organisation: land_registry,
   description: 'update this task'
 )
 
-price_paid_dataset = Dataset.create!(
+# Price paid dataset
+Dataset.create!(
   name: 'price paid data',
   title: 'Price Paid data for all London Boroughs',
   summary: 'Price Paid Data tracks the residential property sales in England and Wales that are lodged with HM Land Registry for registration. ',
   organisation: land_registry
 )
 
-hmrc_spending_dataset = Dataset.create!(
+# HMRC Spending dataset
+Dataset.create!(
   name: 'HMRC spending',
   title: 'HMRC spending over £25000',
   summary: 'Monthly details of HMRC’s spending with suppliers covering transactions over £25,000',
   organisation: land_registry
 )
 
-council_tax_bands = Dataset.create!(
+# Council tax bands
+Dataset.create!(
   name: 'Council Tax',
   title: 'Council Tax bands for London',
   summary: 'Council tax bands for the current year',
   organisation: land_registry
 )
 
-for i in 1..5
+(1..5).each do |i|
   Dataset.create!(
-  name: "LR_Dataset_#{i} name",
-  title: "LR_Dataset_#{i} title",
-  summary: "LR_Dataset_#{i} summary",
-  organisation: land_registry
+    name: "LR_Dataset_#{i} name",
+    title: "LR_Dataset_#{i} title",
+    summary: "LR_Dataset_#{i} summary",
+    organisation: land_registry
   )
 end
 
-for i in 1..5
+(1..5).each do |i|
   Dataset.create!(
   name: "HMRC_Dataset_#{i} name",
   title: "HMRC_Dataset_#{i} title",
