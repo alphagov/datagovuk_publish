5b0df33e425fe29d38be034a19d4e490247dd651
Ross Jones <ross@servercode.co.uk>
2017-06-21 13:21:31 +0100

    Change rememberable for user model instead of removing html

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,11 @@ class User < ApplicationRecord
   has_and_belongs_to_many :organisations
   belongs_to :primary_organisation, class_name: 'Organisation',
     foreign_key: 'primary_organisation_id', optional: true
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :invitable, :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :trackable, :validatable
 
   validates :email, presence: true
 
