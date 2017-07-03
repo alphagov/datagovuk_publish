c798fb8f8607642c0a3af9e75c3d1a910cccf20b
Laurence <laurence.smith@digital.cabinet-office.gov.uk>
2017-07-03 16:56:32 +0100

    Add prefills on all forms

--- app/models/datafile.rb
+++ app/models/datafile.rb
@@ -11,4 +11,57 @@ class Datafile < ApplicationRecord
 
   scope :datalinks,     -> { where(documentation: [false, nil]) }
   scope :documentation, -> { where(documentation: true) }
+
+  def quarter
+    if start_date
+      year_quarter = (start_date.month - 1) / 3
+
+      if year_quarter == 0
+        4
+      else
+        year_quarter
+      end
+    else
+      nil
+    end
+  end
+
+  def dates
+    {
+      start: {
+        day: start_day,
+        month: start_month,
+        year: start_year
+      },
+      end: {
+        day: end_day,
+        month: end_month,
+        year: end_year
+      }
+    }.with_indifferent_access
+  end
+
+  def start_day
+    start_date.try(:day)
+  end
+
+  def start_month
+    start_date.try(:month)
+  end
+
+  def start_year
+    start_date.try(:year)
+  end
+
+  def end_day
+    end_date.try(:day)
+  end
+
+  def end_month
+    end_date.try(:month)
+  end
+
+  def end_year
+    end_date.try(:year)
+  end
 end
