c68b69cae054ed04f0b6f6ad369b139285b7391e
Ross Jones <ross@servercode.co.uk>
2017-07-04 13:46:32 +0100

    Creates a view helper for the FIND_URL
    This is only used on the manage page, and so is only available at
    pages in app/views/manage.

--- app/controllers/manage_controller.rb
+++ app/controllers/manage_controller.rb
@@ -40,7 +40,6 @@ class ManageController < ApplicationController
 
   def set_common_args
     @organisation = current_user.primary_organisation
-    @find_url = ENV["FIND_URL"] || ""
     @q = params[:q]
   end
 
