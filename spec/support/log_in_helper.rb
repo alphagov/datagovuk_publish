56f682434902d898a7320a8c3b83e9c4b599a385
hannako <jessica.jones@digital.cabinet-office.gov.uk>
2017-07-18 19:54:43 +0100

    create_dataset_spec refactor

--- spec/support/log_in_helper.rb
+++ spec/support/log_in_helper.rb
@@ -6,3 +6,11 @@ def create_user_and_sign_in
   fill_in('Password', with: 'password')
   click_button 'Sign in'
 end
+
+def sign_in_user
+  visit '/'
+  click_link 'Sign in'
+  fill_in('user_email', with: 'test@localhost.co.uk')
+  fill_in('Password', with: 'password')
+  click_button 'Sign in'
+end
