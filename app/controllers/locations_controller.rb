c023bcef8c0bea68026aa87c49402b396604132c
Max Froumentin <github@lapin-bl.eu>
2017-07-18 16:53:43 +0100

    Don't show empty brackets when location doesn't have a type

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -7,10 +7,20 @@ class LocationsController < ApplicationController
     if params[:q]
       @query = params[:q].downcase
       @locations = Location.where("name ILIKE CONCAT('%',:search,'%')", {search: @query})
-      @location_strings = @locations.map { | l | l.name + " (" + l.location_type + ")" }
+      @location_strings = @locations.map { |x| format_location x }
       render json: @location_strings
     else
       render json: []
     end
+  end
+
+  private
+
+  def format_location(l)
+    result = l.name
+    if l.location_type && l.location_type != ""
+      result += " (#{l.location_type})"
+    end
+    result
   end
 end
