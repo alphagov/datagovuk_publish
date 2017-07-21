8ff5088db77838215946fbe485375b192adb5544
Laurence <laurence.smith@digital.cabinet-office.gov.uk>
2017-07-21 13:51:44 +0100

    Setup for async publishing

--- app/workers/publishing_worker.rb
+++ app/workers/publishing_worker.rb
@@ -1,7 +1,12 @@
 class PublishingWorker
   include Sidekiq::Worker
 
-  def perform(*args)
-    # Do something
+  def perform(dataset_id)
+    dataset = Dataset.find(dataset_id)
+
+    if dataset.publishable?
+      logger.info "Attempting to publish dataset #{dataset.id}"
+      dataset.__elasticsearch__.index_document
+    end
   end
 end
