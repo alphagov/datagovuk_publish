43971c263ffe697054957e6544062bb5e22324b5
Ross Jones <ross@servercode.co.uk>
2017-08-04 11:43:12 +0100

    Fix migration
    Appears when the test DB is created it isn't done with migrations

--- db/migrate/20170731141237_add_previews.rb
+++ db/migrate/20170731141237_add_previews.rb
@@ -1,7 +1,7 @@
 class AddPreviews < ActiveRecord::Migration[5.1]
   def change
     create_table :previews do |t|
-      t.references :link, foreign_key: true
+      t.references :datafile, foreign_key: true
       t.json :content
       t.timestamps
     end
