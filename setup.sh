a4db5a0fe50520e68cc13ca3e74e9240fbbd91ce
Max Froumentin <github@lapin-bl.eu>
2017-09-13 15:27:08 +0100

    small changes:
    - add # at the start
    - use curl, as it's likelier to be installed
    - reset the db in case it was already there
    - clean up the big json files

--- setup.sh
+++ setup.sh
@@ -1,7 +1,11 @@
-!/usr/bin/env bash
+#!/usr/bin/env bash
 
 print_warning() {
   echo -e "\033[1;33m"$1"\033[00m";
+}
+
+print() {
+  echo -e "\033[1;32m"$1"\033[00m";
 }
 
 print_error() {
@@ -10,19 +14,24 @@ print_error() {
 
 export SECRET_KEY_BASE="foobar"
 bundle install
-rails db:create db:migrate db:seed
+rails db:reset db:migrate db:seed
 
-if type wget > /dev/null 2>&1; then
-  print_warning "wget found at '`which wget`', moving on."
-  wget https://data.gov.uk/data/dumps/data.gov.uk-ckan-meta-data-latest.organizations.jsonl.gz
-  wget https://data.gov.uk/data/dumps/data.gov.uk-ckan-meta-data-latest.v2.jsonl.gz
-else
-  print_error "Please install wget and run this script again."
-fi
+curl -O https://data.gov.uk/data/dumps/data.gov.uk-ckan-meta-data-latest.organizations.jsonl.gz
+curl -O https://data.gov.uk/data/dumps/data.gov.uk-ckan-meta-data-latest.v2.jsonl.gz
 
 gunzip data.gov.uk-ckan-meta-data-latest.organizations.jsonl.gz
 gunzip data.gov.uk-ckan-meta-data-latest.v2.jsonl.gz
 
+print "Adding locations"
 rails import:locations["lib/seeds/locations.csv"]
+
+print "Adding organisations"
 rails import:organisations["data.gov.uk-ckan-meta-data-latest.organizations.jsonl"]
+
+print "Adding datasets"
 rails import:datasets["data.gov.uk-ckan-meta-data-latest.v2.jsonl"]
+
+print "Cleaning up"
+rm data.gov.uk-ckan-meta-data-latest.organizations.jsonl data.gov.uk-ckan-meta-data-latest.v2.jsonl.gz
+
+print "All done."
