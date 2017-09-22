7f5c2084a47cf5e562bab25d39b4cd79c26bcc82
TobyRet <toby.retallick@gmail.com>
2017-09-22 09:17:56 +0100

    Move elastic timeout to elastic config. Don't mutate the config [slaps hand]

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -4,14 +4,13 @@ CONFIG_PATH = "#{Rails.root}/config/elasticsearch.yml".freeze
 TEMPLATE = ERB.new File.new(CONFIG_PATH).read
 ELASTIC_CONFIG = YAML.load(TEMPLATE.result(binding))[ENV['RAILS_ENV']]
 
-config = {
-  host: ELASTIC_CONFIG['host'],
-  transport_options: {
-    request: { timeout: 5 }
-  }
-}
+def log(server, filepath)
+  Rails.logger.info "Configuring Elasticsearch on PAAS.\n
+  Elasticsearch host: #{server}\n
+  Elasticsearch cert file path: #{filepath}"
+end
 
-if ELASTIC_CONFIG['vcap_services'].present?
+def es_config_production
   vcap = JSON.parse(ELASTIC_CONFIG['vcap_services'])
 
   es_server = vcap['elasticsearch'][0]['credentials']['uri'].chomp('/')
@@ -20,13 +19,35 @@ if ELASTIC_CONFIG['vcap_services'].present?
   es_cert_file.puts(es_cert)
   es_cert_file.close
 
-  Rails.logger.info "Configuring Elasticsearch on PAAS.\n
-  Elasticsearch host: #{es_server}\n
-  Elasticsearch cert file path: #{es_cert_file.path}"
+  log(es_server, es_cert_file.path)
 
-  config[:host] = es_server
-  config[:transport_options][:ssl][:ca_file] = es_cert_file.path
+  {
+    host: es_server,
+    transport_options: {
+      request: {
+        timeout: ELASTIC_CONFIG['elastic_timeout']
+      },
+      ssl: {
+        ca_file: es_cert_file.path
+      }
+    }
+  }
 end
+
+def es_config_non_production
+  {
+    host: ELASTIC_CONFIG['host'],
+    transport_options: {
+      request: {
+        timeout: ELASTIC_CONFIG['elastic_timeout']
+      }
+    }
+  }
+end
+
+config = ELASTIC_CONFIG['vcap_services'].present? ?
+            es_config_production :
+            es_config_non_production
 
 ELASTIC = Elasticsearch::Client.new(config)
 Elasticsearch::Model.client = ELASTIC
