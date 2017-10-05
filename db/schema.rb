c32932724e1219ed3a93cc72dc7d630ebba138a8
Laurent Curau <laurent.curau@digital.cabinet-office.gov.uk>
2017-10-05 11:33:29 +0100

    Fix schema version

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171004100641) do
+ActiveRecord::Schema.define(version: 2017071231151258) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
