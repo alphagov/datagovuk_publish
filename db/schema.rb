4fa2847ad379a4dca83370a14b587185097aff80
Max Froumentin <github@lapin-bl.eu>
2017-10-05 17:11:53 +0100

    Remove unnecessary extension as it breaks PaaS

--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2017071231151258) do
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
