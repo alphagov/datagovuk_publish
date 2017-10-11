fb30202a7b999bb6f362dc6569c980df5d30d83e
Laurent Curau <laurent.curau@digital.cabinet-office.gov.uk>
2017-10-11 16:16:50 +0100

    Quotes quotes quotes

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -2,14 +2,14 @@ REDIS_CONFIG = Rails.application.config_for(:redis)
 
 Sidekiq.configure_server do |config|
   config.redis = {
-    url: "redis://#{REDIS_CONFIG[:host]}:#{REDIS_CONFIG[:port]}/12",
-    password: REDIS_CONFIG[:password]
+    url: "redis://#{REDIS_CONFIG['host']}:#{REDIS_CONFIG['port']}/12",
+    password: REDIS_CONFIG['password']
   }
 end
 
 Sidekiq.configure_client do |config|
   config.redis = {
-    url: "redis://#{REDIS_CONFIG[:host]}:#{REDIS_CONFIG[:port]}/12",
-    password: REDIS_CONFIG[:password]
+    url: "redis://#{REDIS_CONFIG['host']}:#{REDIS_CONFIG['port']}/12",
+    password: REDIS_CONFIG['password']
   }
 end
