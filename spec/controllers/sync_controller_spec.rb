2263ef2badf6bf4e1d8bcafde8c797b01debbdae
Toby Retallick <toby.retallick@gmail.com>
2017-10-12 09:52:30 +0100

    FIX - Legacy Sync (#364)
    - Revert POST to GET request to sidestep CSRF error. Not ideal but
    hoping this is ok given this is a temporary endpoint with limited
    security risks

--- spec/controllers/sync_controller_spec.rb
+++ spec/controllers/sync_controller_spec.rb
@@ -4,7 +4,7 @@ require './lib/sync/legacy_datasets'
 describe SyncController, type: :controller do
   it 'invokes the sync legacy dataset rake task' do
     allow_any_instance_of(LegacyDataSync).to receive(:run).and_return true
-    post :legacy
+    get :legacy
     assert_response :success
   end
 end
