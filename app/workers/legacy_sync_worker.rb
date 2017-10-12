0adc90a424a92c3535e81046cb71e0960eeaea31
hannako <jessica.jones@digital.cabinet-office.gov.uk>
2017-10-12 23:48:06 +0100

    only push updates to data.gov.uk on production, else test.data.gov.uk

--- app/workers/legacy_sync_worker.rb
+++ app/workers/legacy_sync_worker.rb
@@ -5,7 +5,8 @@ class LegacySyncWorker
 
   def perform(id)
     json = Dataset.find(id).ckanify_metadata.to_json
-    server = "https://test.data.gov.uk"
+    server =
+      Rails.env.production? ? "https://data.gov.uk" : "https://test.data.gov.uk"
     url = "#{ server }/api/3/action/package_patch"
     headers = {Authorization: "6c25593f-19fe-4064-a451-ddc0aff562d9"}
     RestClient.post(url,json,headers)
