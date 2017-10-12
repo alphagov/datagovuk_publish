39d52d242d7ead398054e250cec362524bb603e1
Max Froumentin <github@lapin-bl.eu>
2017-10-12 17:16:00 +0100

    Remove extension that's crept back again

--- db/schema.rb
+++ db/schema.rb
@@ -12,9 +12,6 @@
 
 ActiveRecord::Schema.define(version: 2017071231151258) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
     t.text "body"
