e865e0e8eee5d27ec5f3e91977ee614f3c04e15e
Laurent Curau <laurent.curau@digital.cabinet-office.gov.uk>
2017-10-18 13:23:51 +0100

    Treat links and docs as resources under datasets

--- config/routes.rb
+++ config/routes.rb
@@ -19,21 +19,15 @@ Rails.application.routes.draw do
   get 'api/start_legacy_sync', to: 'sync#legacy'
 
   resources :datasets do
-    get 'show/:id', to: 'datasets#show'
+    resources :links do
+      get 'confirm_delete', on: :member
+    end
+
+    resources :docs do
+      get 'confirm_delete', on: :member
+    end
 
     member do
-      resources :links, param: :file_id do
-        member do
-          get 'confirm_delete', to: 'links#confirm_delete'
-        end
-      end
-
-      resources :docs, param: :file_id do
-        member do
-          get 'confirm_delete', to: 'docs#confirm_delete'
-        end
-      end
-
       scope module: :datasets do
         resource :licence
         resource :location
