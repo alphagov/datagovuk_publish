d7a7c1260b091f63ead5cf7f38e9f7a1e7f37867
hannako <jessica.jones@digital.cabinet-office.gov.uk>
2017-10-26 15:13:41 +0100

    add format to datafiles, if its nil. Legacy validates on the presence of format and some of our datafiles have nil at the moment. This is a temporary fix which can be reversed once the LinkChecker Rake task is run on import

--- app/models/datafile.rb
+++ app/models/datafile.rb
@@ -8,6 +8,9 @@ class Datafile < ApplicationRecord
   validates_with UrlValidator
 
   before_save :set_uuid
+  before_save :set_format
+
+  FILE_TYPES = ['html', 'csv', 'pdf', 'aspx', 'odt', 'xml', 'wms', 'xls','wfs','json', 'rdf', 'jpeg']
 
   def set_uuid
     if self.uuid.blank?
@@ -15,6 +18,23 @@ class Datafile < ApplicationRecord
     end
   end
 
+  def set_format
+    if self.format.blank?
+      self.format = get_format
+    end
+  end
+
   private
+
+  def get_format
+    begin
+      response = RestClient.head self.url
+    rescue
+      RestClient::ExceptionWithResponse
+    end
+    file_format = response.headers[:content_type].downcase
+    FILE_TYPES.map{ |type| file_format.include?(type)}
+    FILE_TYPES.fetch(0,"")
+  end
 
 end
