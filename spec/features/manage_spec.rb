20aca2c3e62f1353c5fcb2f5bff34979c72e76ab
Laurent Curau <laurent.curau@digital.cabinet-office.gov.uk>
2017-11-02 13:11:39 +0000

    Paginate datasets on /manage page

--- spec/features/manage_spec.rb
+++ spec/features/manage_spec.rb
@@ -1,16 +1,16 @@
 require 'rails_helper'
 
 describe "managing datasets" do
-  let(:land) { FactoryGirl.create(:organisation, name: 'land-registry', title: 'Land Registry') }
-  let(:user) { FactoryGirl.create(:user, primary_organisation: land) }
-  let(:price_paid_dataset) { FactoryGirl.create(:dataset, organisation: land, owner: user, name: 'price paid data', title: 'Price Paid data for all London Boroughs', summary: 'Price Paid Data tracks the residential property sales in England and Wales that are lodged with HM Land Registry for registration. ')}
-  let(:searchable) { FactoryGirl.create(:dataset, organisation: land, owner: user, name: 'searchable', title: 'Find data here',summary: 'A fake dataset for search ')}
+  let(:organisation) { FactoryGirl.create(:organisation) }
+  let(:user) { FactoryGirl.create(:user, primary_organisation: organisation) }
+  let(:dataset_1) { FactoryGirl.create(:dataset, title: "Cats per square mile", organisation: organisation) }
+  let(:dataset_2) { FactoryGirl.create(:dataset, title: "Dogs per square mile", organisation: organisation) }
 
   before(:each) do
     user
     sign_in_user
-    price_paid_dataset
-    searchable
+    dataset_1
+    dataset_2
   end
 
   it "after login" do
@@ -22,21 +22,21 @@ describe "managing datasets" do
     expect(page).to have_selector(%(table), count: 0)
 
     # Expect to see the table with datasets in it.
-    click_link 'Land Registry datasets'
-    expect(page).to have_content('Price Paid data for all London Boroughs')
+    click_link "#{organisation.title} datasets"
+    expect(page).to have_content(dataset_1.title)
     expect(page).to have_selector(%(table), count: 1)
   end
 
   it "can do a search" do
     click_link 'Manage datasets'
-    click_link 'Land Registry datasets'
+    click_link "#{organisation.title} datasets"
 
     # expect 2 datasets to be displayed
     within('#dataset-list') do
       expect(page).to have_selector(%(th), count: 2)
     end
 
-    fill_in('q', with: "find")
+    fill_in('q', with: "#{dataset_1.title}")
     click_button 'Search'
 
     # We expect only a single result now
@@ -53,4 +53,18 @@ describe "managing datasets" do
     expect(page).to have_selector(%(th), count: 0)
   end
 
+  it "paginates datasets" do
+    FactoryGirl.create(:dataset, organisation: organisation) # create a third dataset
+    datasets_per_page = 2
+
+    visit manage_organisation_path(per: datasets_per_page)
+    within('#dataset-list') do
+      expect(page).to have_selector(%(th), count: 2)
+    end
+
+    click_link '2'
+    within('#dataset-list') do
+      expect(page).to have_selector(%(th), count: 1)
+    end
+  end
 end
