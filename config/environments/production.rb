fac0adcffe354c48dafac3792586e5c3dade16d5
Laurence <laurence.smith@digital.cabinet-office.gov.uk>
2017-11-17 10:31:50 +0000

    Re-enable logger formatter but use ActiveSupport logger

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,7 +74,7 @@ Rails.application.configure do
   config.active_support.deprecation = :notify
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
-  # config.log_formatter = ::Logger::Formatter.new
+  config.log_formatter = ::Logger::Formatter.new
 
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
@@ -83,7 +83,8 @@ Rails.application.configure do
   if ENV["RAILS_LOG_TO_STDOUT"].present?
     logger           = ActiveSupport::Logger.new(STDOUT)
     logger.formatter = config.log_formatter
-    config.logger    = ActiveSupport::TaggedLogging.new(logger)
+    # config.logger    = ActiveSupport::TaggedLogging.new(logger)
+    config.logger = logger
   end
 
   # Do not dump schema after migrations.
