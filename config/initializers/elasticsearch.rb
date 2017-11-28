2664225374491b4505a21c32ce0f37c54665ba93
Laurent Curau <laurent.curau@digital.cabinet-office.gov.uk>
2017-11-28 16:09:36 +0000

    Refactor ES initializer

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -1,47 +1,29 @@
 require 'base64'
 
-CONFIG_PATH = Rails.root.join('config', 'elasticsearch.yml')
-TEMPLATE = ERB.new File.new(CONFIG_PATH).read
-
-begin
-  ELASTIC_CONFIG = YAML.load(TEMPLATE.result(binding))[ENV['RAILS_ENV']]
-rescue => e
-  Rails.logger.fatal "Failed to parse elasticsearch yaml configuration. Exiting"
-  Rails.logger.fatal e
-  exit
+def config
+  return es_config_from_host if elastic_config['host'].present?
+  return es_config_from_vcap if elastic_config['vcap_services'].present?
+  Rails.logger.fatal "No elasticsearch environment variables found"
+  return {}
 end
 
-def create_es_cert_file(cert)
-  begin
-    es_cert_file = File.new('elasticsearch_cert.pem', 'w')
-    es_cert_file.write(cert)
-    es_cert_file.close
-  rescue => e
-    Rails.logger.fatal "Failed to write elasticsearch certificate. Exiting"
-    Rails.logger.fatal e
-    exit
-  end
-  es_cert_file
+def es_config_from_host
+  {
+    host: elastic_config['host'],
+    transport_options: {
+      request: {
+        timeout: elastic_config['elastic_timeout']
+      }
+    }
+  }
 end
 
 def es_config_from_vcap
-  begin
-    vcap = JSON.parse(ELASTIC_CONFIG['vcap_services'])
-    es_server = vcap['elasticsearch'][0]['credentials']['uri'].chomp('/')
-    es_cert = Base64.decode64(vcap['elasticsearch'][0]['credentials']['ca_certificate_base64'])
-  rescue => e
-    Rails.logger.fatal "Failed to extract ES creds from VCAP_SERVICES. Exiting"
-    Rails.logger.fatal ELASTIC_CONFIG['vcap_services']
-    Rails.logger.fatal e
-    exit
-  end
-  es_cert_file = create_es_cert_file(es_cert)
-
   {
     host: es_server,
     transport_options: {
       request: {
-        timeout: ELASTIC_CONFIG['elastic_timeout']
+        timeout: elastic_config['elastic_timeout']
       },
       ssl: {
         ca_file: es_cert_file.path
@@ -50,28 +32,42 @@ def es_config_from_vcap
   }
 end
 
-def es_config_from_host
-  {
-    host: ELASTIC_CONFIG['host'],
-    transport_options: {
-      request: {
-        timeout: ELASTIC_CONFIG['elastic_timeout']
-      }
-    }
-  }
+def vcap
+  JSON.parse(elastic_config['vcap_services'])
 end
 
+def elastic_config
+  path = Rails.root.join('config', 'elasticsearch.yml')
+  elastic_yaml_file = ERB.new File.new(path).read
 
-if ELASTIC_CONFIG['host']
-  config = es_config_from_host
-elsif ELASTIC_CONFIG['vcap_services']
-  config = es_config_from_vcap
-else
-  Rails.logger.fatal "No elasticsearch environment variables found"
-  config = nil
+  begin
+    YAML.load(elastic_yaml_file.result(binding))[ENV['RAILS_ENV']]
+  rescue => e
+    Rails.logger.fatal "Failed to parse elasticsearch yaml configuration. Exiting"
+    Rails.logger.fatal e
+    exit
+  end
 end
+
+def es_server
+  vcap['elasticsearch'][0]['credentials']['uri'].chomp('/')
+end
+
+def es_cert_file
+  begin
+    File.open('elasticsearch_cert.pem', 'w') { |file| file.write es_cert }
+  rescue => e
+    Rails.logger.fatal "Failed to write elasticsearch certificate. Exiting"
+    Rails.logger.fatal e
+    exit
+  end
+end
+
+def es_cert
+  Base64.decode64(vcap['elasticsearch'][0]['credentials']['ca_certificate_base64'])
+end
+
+Elasticsearch::Model.client = Elasticsearch::Client.new(config)
 
 Rails.logger.info "Elasticsearch config:"
 Rails.logger.info config
-
-Elasticsearch::Model.client = Elasticsearch::Client.new(config)
