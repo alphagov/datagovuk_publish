0d8db2fb9166068a51de2251a05fad1f1154607d
TobyRet <toby.retallick@gmail.com>
2017-12-05 11:15:56 +0000

    Fix test for beta sync service

--- spec/services/legacy/integration/beta_sync_service_spec.rb
+++ spec/services/legacy/integration/beta_sync_service_spec.rb
@@ -1,16 +1,16 @@
 require 'rails_helper'
-# require 'util/metadata_tools'
 
-xdescribe Legacy::BetaSyncService do
+describe Legacy::BetaSyncService do
   before do
-    @orgs_cache = Organisation.all.pluck(:uuid, :id).to_h
-    @theme_cache = Theme.all.pluck(:title, :id).to_h
     @modified_datasets_path = 'api/3/action/package_search?q=metadata_modified:[NOW-1DAY%20TO%20NOW]&rows=5000'
     @new_datasets_path = 'api/3/action/package_search?q=metadata_created:[NOW-1DAY%20TO%20NOW]&rows=5000'
-    @legacy_server = double('legacy_server', fetch: '')
-    @logger = double('logger', info: '')
 
-    @beta_sync_service = Legacy::LegacyToBetaSyncService.new(
+    @orgs_cache = Organisation.all.pluck(:uuid, :id).to_h
+    @theme_cache = Theme.all.pluck(:title, :id).to_h
+    @legacy_server = double(:legacy_server)
+    @logger = double(:logger, info: true)
+
+    @beta_sync_service = Legacy::BetaSyncService.new(
       orgs_cache: @orgs_cache,
       theme_cache: @theme_cache,
       logger: @logger,
@@ -20,15 +20,34 @@ xdescribe Legacy::BetaSyncService do
 
   describe 'There are modified and new legacy datasets to be imported' do
     it 'Imports legacy datasets' do
-      package = {someDataset: {name: 'Awesome data'}}
-      response = {'result'=> {'results' => [package]}}
-      first_dataset = double('first dataset')
-      second_dataset = double('second dataset')
+      id_for_first_legacy_dataset = 1
+      id_for_second_legacy_dataset = 2
 
-      allow(@legacy_server).to receive(:get).and_return(response)
-      allow(Dataset).to receive(:find_by).and_return([first_dataset, second_dataset])
-      # allow(MetadataTools).to receive(:persist)
-      # allow(MetadataTools).to receive(:index)
+      first_legacy_dataset = {'name'  => 'Awesome data', 'id' => id_for_first_legacy_dataset}
+      second_legacy_dataset = {'name' => 'More awesome data', 'id' => id_for_second_legacy_dataset}
+
+      first_response = {'result'=> {'results' => [first_legacy_dataset]}}
+      second_response = {'result'=> {'results' => [second_legacy_dataset]}}
+
+      dataset_import_service = double(:dataset_importer_service, run: true)
+      dataset_index_service = double(:dataset_indexer_service, index: true)
+
+      allow(@legacy_server).to receive(:get).and_return(first_response, second_response)
+
+      expect(Legacy::DatasetImportService).to receive(:new)
+                                                .with(first_legacy_dataset, @orgs_cache, @theme_cache)
+                                                .once
+                                                .and_return(dataset_import_service)
+
+      expect(Legacy::DatasetImportService).to receive(:new)
+                                                .with(second_legacy_dataset, @orgs_cache, @theme_cache)
+                                                .once
+                                                .and_return(dataset_import_service)
+
+      expect(Legacy::DatasetIndexService).to receive(:new)
+                                               .twice
+                                               .and_return(dataset_index_service)
+
 
       @beta_sync_service.run
 
@@ -42,15 +61,19 @@ xdescribe Legacy::BetaSyncService do
               .with(@new_datasets_path)
               .once
 
-      # expect(MetadataTools)
-      #   .to have_received(:persist)
-      #         .exactly(2).times
-      #         .with(package, @orgs_cache, @theme_cache)
-      #
-      # expect(MetadataTools)
-      #   .to have_received(:index)
-      #         .exactly(2).times
-      #         .with(package)
+      expect(dataset_import_service)
+        .to have_received(:run).twice
+
+      expect(dataset_index_service)
+        .to have_received(:index)
+              .with(id_for_first_legacy_dataset)
+              .once
+
+      expect(dataset_index_service)
+        .to have_received(:index)
+              .with(id_for_second_legacy_dataset)
+              .once
+
     end
 
     describe 'there are no modified or new datasets to be imported' do
@@ -58,8 +81,6 @@ xdescribe Legacy::BetaSyncService do
         response = {'result' => {'results' => []}}
 
         allow(@legacy_server).to receive(:get).and_return(response)
-        # allow(MetadataTools).to receive(:persist)
-        # allow(MetadataTools).to receive(:index)
 
         @beta_sync_service.run
 
@@ -73,11 +94,9 @@ xdescribe Legacy::BetaSyncService do
                 .with(@new_datasets_path)
                 .once
 
-        # expect(MetadataTools)
-        #   .to_not have_received(:persist)
-        #
-        # expect(MetadataTools)
-        #   .to_not have_received(:index)
+        expect_any_instance_of(Legacy::DatasetImportService).to_not receive(:run)
+
+        expect_any_instance_of(Legacy::DatasetIndexService).to_not receive(:index)
       end
     end
   end
