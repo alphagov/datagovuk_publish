cbaa480a035f7056555c39bdcf7c773788657d3a
Andrew Garner <andrew@andrewgarner.com>
2018-02-09 12:22:01 +0000

    Fix Lint/IneffectiveAccessModifier offenses

--- app/models/legacy/server.rb
+++ app/models/legacy/server.rb
@@ -1,28 +1,30 @@
 class Legacy::Server
-  def self.url_for(resource_name:, action:)
-    URI.join(self.host, ENDPOINTS[resource_name][action]).to_s
-  end
+  class << self
+    def url_for(resource_name:, action:)
+      URI.join(self.host, ENDPOINTS[resource_name][action]).to_s
+    end
 
-  def self.get(path)
-    url = URI::join(host, path).to_s
-    get_json url
-  end
+    def get(path)
+      url = URI::join(host, path).to_s
+      get_json url
+    end
 
-  def self.headers
-    { Authorization: ENV.fetch('LEGACY_API_KEY') }
-  end
+    def headers
+      { Authorization: ENV.fetch('LEGACY_API_KEY') }
+    end
 
-  private
+    private
 
-  def self.host
-    ENV.fetch('LEGACY_HOST')
-  end
+    def host
+      ENV.fetch('LEGACY_HOST')
+    end
 
-  def self.get_json(url)
-    response = RestClient.get url
-    return JSON.parse(response.body)
-  rescue RestClient::ExceptionWithResponse
-    Raven.capture_exception "Failed to make the request to #{url}"
-    return nil
+    def get_json(url)
+      response = RestClient.get url
+      return JSON.parse(response.body)
+    rescue RestClient::ExceptionWithResponse
+      Raven.capture_exception "Failed to make the request to #{url}"
+      return nil
+    end
   end
 end
