e4d92688edf357824e95347f4c766795ebeba09e
hannako <jessica.jones@digital.cabinet-office.gov.uk>
2018-02-22 17:53:53 +0000

    replace bgd cf plugin with autopilot

--- scripts/deploy-staging.sh
+++ scripts/deploy-staging.sh
@@ -2,10 +2,10 @@
 
 rm manifest.yml || true
 ln -s staging-app-manifest.yml manifest.yml
-cf bgd publish-data-beta-staging staging-app-manifest.yml
+cf zero-downtime-push publish-data-beta-staging -f manifest.yml
 
 rm manifest.yml
 ln -s staging-worker-manifest.yml manifest.yml
-cf bgd publish-data-beta-staging-worker staging-worker-manifest.yml
+cf zero-downtime-push publish-data-beta-staging-worker -f manifest.yml
 
 rm manifest.yml
