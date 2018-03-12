6a17c0f74ca02909040eae5073d093ff6b311d35
Andrew Garner <andrew@andrewgarner.com>
2018-03-12 09:39:53 +0000

    Configure autopilot to show application log during application start
    If the application fails to start we are at a loss to why!

--- scripts/deploy-staging.sh
+++ scripts/deploy-staging.sh
@@ -2,10 +2,10 @@
 
 rm manifest.yml || true
 ln -s staging-app-manifest.yml manifest.yml
-cf zero-downtime-push publish-data-beta-staging -f manifest.yml
+cf zero-downtime-push publish-data-beta-staging -f manifest.yml --show-app-log=true
 
 rm manifest.yml
 ln -s staging-worker-manifest.yml manifest.yml
-cf zero-downtime-push publish-data-beta-staging-worker -f manifest.yml
+cf zero-downtime-push publish-data-beta-staging-worker -f manifest.yml --show-app-log=true
 
 rm manifest.yml
