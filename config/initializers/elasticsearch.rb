9775a2cb0ada560bf2b40cda93832925a1a2508c
Andrew Garner <andrew@andrewgarner.com>
2018-03-12 09:32:25 +0000

    Use Rails' custom configuration support to configure Elasticsearch
    Rails has built-in support for custom configuration. This takes care
    of parsing configuration files, and selecting the correct
    configuration values for the current environment.
    
    This changes the way we configure Elasticsearch to use this strategy
    instead of rolling our own implementation.
    
    Previously implemented with https://github.com/alphagov/datagovuk_find/commit/3a8b8b43cc41181d304691f0c04911fc18b2bda2#diff-6e42962516179770cff5c2154c51baed

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -1,16 +1,5 @@
 require 'base64'
 
-CONFIG_PATH = Rails.root.join('config', 'elasticsearch.yml')
-TEMPLATE = ERB.new File.new(CONFIG_PATH).read
-
-begin
-  ELASTIC_CONFIG = YAML.load(TEMPLATE.result(binding))[ENV['RAILS_ENV']]
-rescue => e
-  Rails.logger.fatal "Failed to parse elasticsearch yaml configuration. Exiting"
-  Rails.logger.fatal e
-  exit
-end
-
 def create_es_cert_file(cert)
   begin
     es_cert_file = File.new('elasticsearch_cert.pem', 'w')
@@ -26,7 +15,7 @@ end
 
 def es_config_from_vcap
   begin
-    vcap = JSON.parse(ELASTIC_CONFIG['vcap_services'])
+    vcap = JSON.parse(Rails.configuration.elasticsearch['vcap_services'])
     es_servers = vcap['elasticsearch'][0]['credentials']['uris'].map do |uri|
       uri.chomp('/')
     end
@@ -42,7 +31,7 @@ def es_config_from_vcap
     host: es_servers,
     transport_options: {
       request: {
-        timeout: ELASTIC_CONFIG['elastic_timeout']
+        timeout: Rails.configuration.elasticsearch['elastic_timeout']
       },
       ssl: {
         ca_file: es_cert_file.path
@@ -53,19 +42,19 @@ end
 
 def es_config_from_host
   {
-    host: ELASTIC_CONFIG['host'],
+    host: Rails.configuration.elasticsearch['host'],
     transport_options: {
       request: {
-        timeout: ELASTIC_CONFIG['elastic_timeout']
+        timeout: Rails.configuration.elasticsearch['elastic_timeout']
       }
     }
   }
 end
 
 
-if ELASTIC_CONFIG['host']
+if Rails.configuration.elasticsearch['host']
   config = es_config_from_host
-elsif ELASTIC_CONFIG['vcap_services']
+elsif Rails.configuration.elasticsearch['vcap_services']
   config = es_config_from_vcap
 else
   Rails.logger.fatal "No elasticsearch environment variables found"
