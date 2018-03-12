618ae1d8c1fe9b047ffb3c9e3e0ca8e00f536bc0
Andrew Garner <andrew@andrewgarner.com>
2018-03-12 09:44:25 +0000

    Exit deploy script immediately if a step fails
    We want the deployment to fail if any of the deploy steps fail.
    
    See:
    
    - https://docs.travis-ci.com/user/customizing-the-build/#Implementing-Complex-Build-Steps
    - https://explainshell.com/explain?cmd=set+-e

--- scripts/deploy-staging.sh
+++ scripts/deploy-staging.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+set -e
+
 cf zero-downtime-push publish-data-beta-staging -f staging-app-manifest.yml --show-app-log=true
 cf zero-downtime-push publish-data-beta-staging-worker -f staging-worker-manifest.yml --show-app-log=true
