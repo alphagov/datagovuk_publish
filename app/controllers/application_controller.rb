9f8f3e61511d12e82b513cd48a1fa1ecb80b296d
Ben Thorner <ben.thorner@digital.cabinet-office.gov.uk>
2018-04-18 15:55:16 +0100

    Remove raven dependency on internal orgs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,10 +18,10 @@ private
 
   def set_raven_context
     Raven.user_context(id: current_user&.id,
-                       name: current_user &.name,
+                       name: current_user&.name,
                        email: current_user&.email,
-                       organisation_id: current_user&.primary_organisation&.id,
-                       organisation: current_user&.primary_organisation&.name)
+                       organisation_slug: current_user&.organisation_slug,
+                       organisation_content_id: current_user&.organisation_content_id)
 
     Raven.extra_context(params: params.to_unsafe_h,
                         url: request.url,
