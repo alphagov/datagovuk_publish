0bf4e1b757f9db2c7f9ba2d7c5ec02e38464cf61
Ben Thorner <ben.thorner@digital.cabinet-office.gov.uk>
2018-04-18 16:13:07 +0100

    Cleanup the rspec spec_helper
    Most of the config in this file was unnecessary. I've also extraxted the
    helper code to a new elastic_helper in spec/support.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -12,89 +12,23 @@ WebMock.disable_net_connect!(allow_localhost: true)
 Sidekiq::Logging.logger = Rails.logger
 Sidekiq::Testing.inline!
 
-SimpleCov.start do
-  add_filter "/app/admin/"
-  add_filter "/spec/"
-end
+SimpleCov.start
 
 RSpec.configure do |config|
+  config.include FactoryGirl::Syntax::Methods
+
   config.before(:each) do
     delete_index
     create_index
   end
 
-  config.after(:each) do
-    delete_index
-  end
-
-  config.include FactoryGirl::Syntax::Methods
-  config.expect_with :rspec do |expectations|
-    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
-  end
-
-  config.mock_with :rspec do |mocks|
-    mocks.verify_partial_doubles = true
-  end
-
-  config.shared_context_metadata_behavior = :apply_to_host_groups
-
-  if config.files_to_run.one?
-    config.default_formatter = "doc"
-  end
-
-  config.profile_examples = 10
   config.order = :random
-  Kernel.srand config.seed
-
-  config.before(:suite) do
-    DatabaseCleaner.clean_with(:truncation)
-  end
 
   config.before(:each) do
     allow_any_instance_of(UrlValidator).to receive(:validPath?).and_return(true)
   end
 
-  config.before(:each) do
-    DatabaseCleaner.strategy = :transaction
-  end
-
-  config.before(:each) do
-    DatabaseCleaner.start
-  end
-
-  config.after(:each) do
-    DatabaseCleaner.clean
-  end
-
-end
-
-def mappings
-  DatasetsIndexerService::INDEX_MAPPING
-end
-
-def delete_index
-  if Rails.env == "test"
-    begin
-      ELASTIC.indices.delete index: "datasets-test"
-    rescue
-      Rails.logger.debug("No test search index to delete")
-    end
-  end
-end
-
-def create_index
-  if Rails.env == "test"
-    begin
-      Rails.logger.info("Creating datasets-test index")
-
-      ELASTIC.indices.create(
-        index: "datasets-test",
-        body: {
-          mappings: mappings
-        }
-      )
-    rescue
-      Rails.logger.debug("Could not create datasets-test index")
-    end
+  config.around(:each) do |example|
+    DatabaseCleaner.cleaning { example.run }
   end
 end
