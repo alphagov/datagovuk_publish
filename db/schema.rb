26a05e5a4d106f4a398fc6710e08e6aa3c284f53
Ben Thorner <ben.thorner@digital.cabinet-office.gov.uk>
2018-04-25 11:32:32 +0100

    Remove last remains of active admin
    This is causing a bug on Heroku as these stray files can't be loaded
    properly.

--- db/schema.rb
+++ db/schema.rb
@@ -15,20 +15,6 @@ ActiveRecord::Schema.define(version: 2018041810194100) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "active_admin_comments", force: :cascade do |t|
-    t.string "namespace"
-    t.text "body"
-    t.string "resource_type"
-    t.bigint "resource_id"
-    t.string "author_type"
-    t.bigint "author_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["author_type", "author_id"], name: "index_active_admin_comments_on_author_type_and_author_id"
-    t.index ["namespace"], name: "index_active_admin_comments_on_namespace"
-    t.index ["resource_type", "resource_id"], name: "index_active_admin_comments_on_resource_type_and_resource_id"
-  end
-
   create_table "audits", force: :cascade do |t|
     t.integer "auditable_id"
     t.string "auditable_type"
