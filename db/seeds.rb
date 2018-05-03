3d8699bb80462c05211c13f026eb8ac580683e0f
Ben Thorner <ben.thorner@digital.cabinet-office.gov.uk>
2018-05-03 11:13:29 +0100

    Use GDS as seed org so PR apps work for us

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,15 +19,15 @@ Topic.create([
 ])
 
 puts 'Seeding organisations'
-land_registry = Organisation.create(name: 'land-registry',
-                                    title: 'Land Registry',
-                                    govuk_content_id: SecureRandom.uuid)
+org = Organisation.create(name: 'government-digital-service',
+                                    title: 'Government Digital Service',
+                                    govuk_content_id: 'af07d5a5-df63-4ddc-9383-6a666845ebe9')
 
 
 puts 'Seeding users'
 User.create(email: 'publisher@example.com',
             name: 'Publisher',
-            primary_organisation: land_registry)
+            primary_organisation: org)
 
 
 puts 'Seeding locations'
