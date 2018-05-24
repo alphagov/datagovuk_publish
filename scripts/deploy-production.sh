14732c365b72c3aeb4e5423d90932a45f89ebe9b
Andrew Garner <andrew@andrewgarner.com>
2018-05-24 12:55:34 +0100

    Update references to production publish application¬ ¬ The application name has changed so that it conforms¬ to conventions needed for production to staging sign-on sync.¬ ¬ See https://trello.com/c/mBViuo1y

--- scripts/deploy-production.sh
+++ scripts/deploy-production.sh
@@ -2,5 +2,5 @@
 
 set -e
 
-cf zero-downtime-push publish-data-beta -f production-app-manifest.yml --show-app-log=true
-cf zero-downtime-push publish-data-beta-worker -f production-worker-manifest.yml --show-app-log=true
+cf zero-downtime-push publish-data-beta-production -f production-app-manifest.yml --show-app-log=true
+cf zero-downtime-push publish-data-beta-production-worker -f production-worker-manifest.yml --show-app-log=true
