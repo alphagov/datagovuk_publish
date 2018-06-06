75c95d41c680d7c3e36e77d3194bade0c6fe3d79
Ben Thorner <ben.thorner@digital.cabinet-office.gov.uk>
2018-06-06 10:08:12 +0100

    Remove unused rake tasks and workers
    There are several workers that are not used anywhere in the code and are
    polluting the namespace. Similarly, there are several rake tasks that
    are never invoked and serve no current value. We should remove these
    until such time as they are ready to be used.

--- app/workers/dataset_import_worker.rb
+++
@@ -1,8 +0,0 @@
-class DatasetImportWorker
-  include Sidekiq::Worker
-  sidekiq_options queue: :importer, retry: false
-
-  def perform(legacy_dataset, orgs_cache, topic_cache)
-    Legacy::DatasetImportService.new(legacy_dataset, orgs_cache, topic_cache).run
-  end
-end
--- app/workers/link_checker_worker.rb
+++
@@ -1,9 +0,0 @@
-class LinkCheckerWorker
-  include Sidekiq::Worker
-  sidekiq_options queue: :link_checker, retry: 3
-
-  def perform(link_id)
-    link = Link.find(link_id)
-    LinkCheckerService.new(link).run
-  end
-end
