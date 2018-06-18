e6c61a5c757d6cb75ff1200c0d131d54c09c0aa6
Ben Thorner <ben.thorner@digital.cabinet-office.gov.uk>
2018-06-18 14:07:58 +0100

    Remove legacy code for CKAN package sync
    The previous sync mechanism is no longer used and can be safely deleted.
    There is still some code that is used for importing legacy
    organisations, which is why the legacy namespace still remains.
    
    The import script no longer tries to import packages, as this tends to
    take a long time and is usually unnecessary. It is still possible to
    manually import packages by running the PackageSyncWorker.

--- spec/services/legacy/api_service_spec.rb
+++ spec/services/legacy/api_service_spec.rb
@@ -4,27 +4,18 @@ describe Legacy::APIService do
   let(:api_service) { Legacy::APIService.new }
 
   before(:each) do
-    dataset = %{
-      {"result": #{File.read('spec/fixtures/legacy_dataset.json')}}
-    }
     organisation = %{
       {"result": #{File.read('spec/fixtures/legacy_organisation.json')}}
     }
 
-    stub_request(:get, "https://test.data.gov.uk/api/3/action/package_show?id=a_dataset").
-      to_return(status: 200, body: dataset)
     stub_request(:get, 'https://test.data.gov.uk/api/3/action/publisher_show?id=a_publisher').
       to_return(status: 200, body: organisation)
-    stub_request(:get, "https://test.data.gov.uk/api/3/action/package_show?id=fake").
+
+    stub_request(:get, 'https://test.data.gov.uk/api/3/action/publisher_show?id=fake').
       to_return(status: 404)
   end
 
   describe "can retrieve valid data" do
-    it "can get dataset info " do
-      result = api_service.dataset_show('a_dataset')
-      expect(result['name']).to eql('local_directgov_services')
-    end
-
     it "can get publisher info " do
       result = api_service.publisher_show('a_publisher')
       expect(result['name']).to eql('2gether-nhs-foundation-trust')
@@ -32,8 +23,8 @@ describe Legacy::APIService do
   end
 
   describe "can handle missing resources" do
-    it "does not explode with missing publisher or dataset" do
-      result = api_service.dataset_show('fake')
+    it "does not explode with missing publisher" do
+      result = api_service.publisher_show('fake')
       expect(result).to be_nil
     end
   end
