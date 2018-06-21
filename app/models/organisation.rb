2adfde41e0d2bf967ba8ea3c0b8b8bba4f94977b
Ben Thorner <ben.thorner@digital.cabinet-office.gov.uk>
2018-06-21 11:26:49 +0100

    Remove redundant org columns and code
    Many of the organisation columns are unused and can be safely removed.
    Also, several methods of the organisation model are unused, together
    with the FriendlyID functionality; this should be removed until it is
    required, to avoid confusion in the future.

--- app/models/organisation.rb
+++ app/models/organisation.rb
@@ -1,28 +1,14 @@
 require 'securerandom'
 
 class Organisation < ApplicationRecord
-  extend FriendlyId
-  has_ancestry
-
   audited
   has_many :tasks, dependent: :destroy
   has_many :datasets, dependent: :destroy
-  friendly_id :slug_candidates, use: :slugged, slug_column: :name
   before_save :set_uuid
 
 private
 
   def set_uuid
     self.uuid = SecureRandom.uuid if self.uuid.blank?
-  end
-
-  def slug_candidates
-    %i[title title_and_sequence]
-  end
-
-  def title_and_sequence
-    slug = title.to_param
-    sequence = Organisation.where("name like ?", "#{slug}-%").count + 2
-    "#{slug}-#{sequence}"
   end
 end
