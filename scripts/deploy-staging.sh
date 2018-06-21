f81a2136bc124574c8d21904f9fcc342f0cba240
Ben Thorner <ben.thorner@digital.cabinet-office.gov.uk>
2018-06-21 13:02:01 +0100

    Remove scripts directory
    This directory contained scripts for deployment, replication and setting
    CF environment variables. The developer docs now include the commands to
    perform a manual deployment; we should not be modifying CF environment
    variables as a common task - they should be controlled using Terraform.
    Replication from a staging/production DB has not been a common task.

--- scripts/deploy-staging.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-set -e
-
-cf zero-downtime-push publish-data-beta-staging -f staging-app-manifest.yml --show-app-log=true
-cf zero-downtime-push publish-data-beta-staging-worker -f staging-worker-manifest.yml --show-app-log=true
