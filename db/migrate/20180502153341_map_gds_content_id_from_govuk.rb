85fe67d6cb1863671a88863594e6609181a31a00
Ben Thorner <ben.thorner@digital.cabinet-office.gov.uk>
2018-06-28 13:30:03 +0100

    Make GOV.UK Signon migrations work from blank DB
    The migrations designed to populate the govuk_content_id field for
    Signon organisations don't work when populating the DB from scratch. In
    addition, there are redundant migrations to support orgs that were only
    used for completed user study; rather than fix these, we should delete
    them.

--- db/migrate/20180502153341_map_gds_content_id_from_govuk.rb
+++ db/migrate/20180502153341_map_gds_content_id_from_govuk.rb
@@ -1,7 +1,8 @@
 class MapGdsContentIdFromGovuk < ActiveRecord::Migration[5.1]
   def change
-    org = Organisation.find_by(name: 'government-digital-service')
-    return unless org
+    org = Organisation.find_or_create_by(uuid: 'af07d5a5-df63-4ddc-9383-6a666845ebe9',
+                                         name: 'government-digital-services')
+
     org.update_attribute(:govuk_content_id, 'af07d5a5-df63-4ddc-9383-6a666845ebe9')
   end
 end
