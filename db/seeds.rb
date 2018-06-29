31a7503f4429a58044bddde23aef391f294ded6a
Ben Thorner <ben.thorner@digital.cabinet-office.gov.uk>
2018-06-29 12:17:36 +0100

    Move data changes to idempotent seeds
    Previously some data changes were done in migrations, while others were
    done in the seeds file. This creates issues where in some environments
    either the migrations or the seeds are run, which caused some data to be
    missing. Using the seeds as the single idempotent mechanism and running
    them for all deployments will ensure longterm data integrity.

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,27 +3,27 @@
 require 'csv'
 
 puts 'Seeding topics'
-Topic.create([
-  { name: "business-and-economy", title: "Business and economy" },
-  { name: "environment", title: "Environment" },
-  { name: "mapping", title: "Mapping" },
-  { name: "crime-and-justice", title: "Crime and justice" },
-  { name: "government", title: "Government" },
-  { name: "society", title: "Society" },
-  { name: "defence", title: "Defence" },
-  { name: "government-spending", title: "Government spending" },
-  { name: "towns-and-cities", title: "Towns and cities" },
-  { name: "education", title: "Education" },
-  { name: "health", title: "Health" },
-  { name: "transport", title: "Transport" },
-])
+Topic.find_or_create_by(name: 'business-and-economy', title: 'Business and economy')
+Topic.find_or_create_by(name: 'environment', title: 'Environment')
+Topic.find_or_create_by(name: 'mapping', title: 'Mapping')
+Topic.find_or_create_by(name: 'crime-and-justice', title: 'Crime and justice')
+Topic.find_or_create_by(name: 'government', title: 'Government')
+Topic.find_or_create_by(name: 'society', title: 'Society')
+Topic.find_or_create_by(name: 'defence', title: 'Defence')
+Topic.find_or_create_by(name: 'government-spending', title: 'Government spending')
+Topic.find_or_create_by(name: 'towns-and-cities', title: 'Towns and cities')
+Topic.find_or_create_by(name: 'education', title: 'Education')
+Topic.find_or_create_by(name: 'health', title: 'Health')
+Topic.find_or_create_by(name: 'transport', title: 'Transport')
 
 puts 'Seeding organisations'
-org = Organisation.create(name: 'government-digital-services',
-                          title: 'Government Digital Service',
-                          govuk_content_id: 'af07d5a5-df63-4ddc-9383-6a666845ebe9')
+org = Organisation.find_or_create_by(uuid: '90aefa0d-0e92-4895-a7fd-c1adb2b3f14f',
+                                     name: 'government-digital-service')
+
+org.update_attribute(:govuk_content_id, 'af07d5a5-df63-4ddc-9383-6a666845ebe9')
+org.update_attribute(:title, 'Government Digital Service')
 
 puts 'Seeding users'
-User.create(email: 'publisher@example.com',
-            name: 'Publisher',
-            primary_organisation: org)
+User.find_or_create_by(email: 'publisher@example.com',
+                       name: 'Publisher',
+                       organisation_content_id: org.govuk_content_id)
