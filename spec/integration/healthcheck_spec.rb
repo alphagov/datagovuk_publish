b23e8805f8aeb59b8e78860d8a5e3310a53bba79
Deborah Chua <deborah.chua@digital.cabinet-office.gov.uk>
2019-05-14 14:30:36 +0100

    Modify healthcheck tests
    We want to check that the healthchecks returns appropriate
    information about our checks.
    
    The tests have been modified so that it takes into account two checks,
    one for the `ckan_v26_package_sync` job and one for
    `ckan_v26_ckan_org_sync`.

--- spec/integration/healthcheck_spec.rb
+++ spec/integration/healthcheck_spec.rb
@@ -4,52 +4,63 @@ RSpec.describe 'Healthcheck', type: :request do
   end
 
   let(:sidekiq_redis) { SidekiqScheduler::RedisManager }
-  let(:message) { "The job 'ckan_v26_package_sync' should run every 10 minutes. It was last run #{when_last_run}." }
-  let(:expected_data) do
-    {
-      status: status,
-      checks: {
-        package_sync: {
-          critical: when_last_run,
-          warning: when_last_run,
-          status: status,
-          message: message
-        }
-      }
-    }
-  end
 
-  before do
-    allow(sidekiq_redis).to receive(:get_job_last_time).and_return(when_last_run)
-  end
-
-  context 'when the healthchecks pass' do
+  context "when the healthchecks pass" do
     let(:when_last_run) { String(10.minutes.ago) }
-    let(:status) { 'ok' }
+    before do
+      allow(sidekiq_redis).to receive(:get_job_last_time).and_return(when_last_run)
+    end
 
     it "returns a status of 'ok'" do
-      get '/healthcheck'
-      expect(data).to eq(expected_data)
+      get "/healthcheck"
+      expect(data.fetch(:status)).to eq("ok")
     end
   end
 
-  context 'when one of the healthchecks is warning' do
+  context "when one of the healthchecks is warning" do
     let(:when_last_run) { String(12.minutes.ago) }
-    let(:status) { 'warning' }
+    before do
+      allow(sidekiq_redis).to receive(:get_job_last_time).and_return(when_last_run)
+    end
 
     it "returns a status of 'warning'" do
       get '/healthcheck'
-      expect(data).to eq(expected_data)
+      expect(data.fetch(:status)).to eq('warning')
     end
   end
 
-  context 'when one of the healthchecks is critical' do
+  context "when one of the healthchecks is critical" do
     let(:when_last_run) { String(70.minutes.ago) }
-    let(:status) { 'critical' }
+    before do
+      allow(sidekiq_redis).to receive(:get_job_last_time).and_return(when_last_run)
+    end
 
     it "returns a status of 'critical'" do
       get '/healthcheck'
-      expect(data).to eq(expected_data)
+      expect(data.fetch(:status)).to eq('critical')
     end
+  end
+
+  let(:when_package_sync_last_run) { String(10.minutes.ago) }
+  let(:when_ckan_org_sync_last_run) { String(24.hours.ago) }
+
+  it "includes useful information about each check" do
+    allow(sidekiq_redis).to receive(:get_job_last_time).with('ckan_v26_package_sync').and_return(when_package_sync_last_run)
+    allow(sidekiq_redis).to receive(:get_job_last_time).with('ckan_v26_ckan_org_sync').and_return(when_ckan_org_sync_last_run)
+
+    get "/healthcheck"
+
+    expect(data.fetch(:checks)).to include(
+      package_sync:  { :critical => when_package_sync_last_run,
+                       :warning => when_package_sync_last_run,
+                       :status => 'ok',
+                       :message => "The job 'ckan_v26_package_sync' should run every 10 minutes. It was last run #{when_package_sync_last_run}."
+                     },
+      CKAN_org_sync: { :critical => when_ckan_org_sync_last_run,
+                       :warning => when_ckan_org_sync_last_run,
+                       :status => 'ok',
+                       :message => "The job 'ckan_v26_ckan_org_sync' should run every 1 day. It was last run #{when_ckan_org_sync_last_run}."
+                     }
+    )
   end
 end
