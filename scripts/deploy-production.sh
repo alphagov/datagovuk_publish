922f6e15c4f87cdd5f44b457829b3c46b07c8d2a
Ken Tsang <ken.tsang@digital.cabinet-office.gov.uk>
2020-01-23 16:41:32 +0000

    Don't deploy the frontend as its not in use

--- scripts/deploy-production.sh
+++ scripts/deploy-production.sh
@@ -2,5 +2,4 @@
 
 set -e
 
-cf zero-downtime-push publish-data-beta-production -f production-app-manifest.yml --show-app-log=true
 cf zero-downtime-push publish-data-beta-production-worker -f production-worker-manifest.yml --show-app-log=true
