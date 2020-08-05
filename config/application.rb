b6c02f14f95a0d4b4c66377e7a937443b342aa1d
pudinha <rogi@skylittlesystem.org>
2020-08-05 15:56:23 +0100

    Update Rails config to 6.0

--- config/application.rb
+++ config/application.rb
@@ -1,12 +1,19 @@
 require_relative "boot"
 
 require "rails"
+# Pick the frameworks you want:
+# require "active_model/railtie"
+# require "active_job/railtie"
 require "active_record/railtie"
+# require "active_storage/engine"
 require "action_controller/railtie"
 require "action_mailer/railtie"
+# require "action_mailbox/engine"
+# require "action_text/engine"
 require "action_view/railtie"
 require "action_cable/engine"
 require "sprockets/railtie"
+# require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -31,7 +38,7 @@ end
 module PublishDataBeta
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.1
+    config.load_defaults 6.0
     config.generators.system_tests = nil
 
     # Don't add a div with field_with_errors class
@@ -45,5 +52,10 @@ module PublishDataBeta
     config.autoload_paths += [Rails.root.join("lib/ckan")]
 
     config.elasticsearch = config_for(:elasticsearch)
+
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration can go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded after loading
+    # the framework and any gems in your application.
   end
 end
