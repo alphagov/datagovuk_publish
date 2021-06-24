7ecf015c9eefcb8fbea387407a5382d160580f9f
kentsang.gds <ken.tsang@digital.cabinet-office.gov.uk>
2021-06-24 12:27:38 +0100

    Update integration environment

--- config/environments/integration.rb
+++ config/environments/integration.rb
@@ -1,1 +1,8 @@
 require_relative "./production"
+
+Rails.application.configure do
+    config.assets.compile = true
+    config.ckan_v26_base_url = "https://ckan.integration.publishing.service.gov.uk"
+    config.test_publisher = ""
+  end
+  
