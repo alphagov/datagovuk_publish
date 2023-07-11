825b517c25f901c9218a815cc7b5bbcc48c4b5fc
kentsanggds <ken.tsang@digital.cabinet-office.gov.uk>
2023-07-11 11:56:51 +0100

    Drop use of legacy connection handling

--- config/application.rb
+++ config/application.rb
@@ -49,6 +49,8 @@ module PublishDataBeta
 
     config.elasticsearch = config_for(:opensearch)
 
+    ActiveRecord.legacy_connection_handling = false
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
