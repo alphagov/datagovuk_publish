16ed5deb75edb5a2f4f74c9b296c0a8bd1940f2f
kentsanggds <ken.tsang@digital.cabinet-office.gov.uk>
2023-09-21 12:28:24 +0100

    Update Integration and Staging CKAN base_url

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -2,6 +2,6 @@ require_relative "./production"
 
 Rails.application.configure do
   config.assets.compile = true
-  config.ckan_v26_base_url = "https://ckan.staging.publishing.service.gov.uk"
+  config.ckan_v26_base_url = "https://ckan.eks.staging.govuk.digital"
   config.test_publisher = ""
 end
