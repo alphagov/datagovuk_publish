601d72e36c20d28a1a180b965ac55d6916140d53
kentsanggds <ken.tsang@digital.cabinet-office.gov.uk>
2023-11-01 13:40:01 +0000

    Revert "Increase MAX_DELETIONS to 16495"

--- app/lib/ckan/v26/depaginator.rb
+++ app/lib/ckan/v26/depaginator.rb
@@ -2,7 +2,7 @@ module CKAN
   module V26
     class Depaginator
       include CKAN::Modules::URLBuilder
-      MAX_DELETIONS = 16_495
+      MAX_DELETIONS = 100
 
       def self.depaginate(*args, **kwargs)
         new(*args, **kwargs).depaginate
